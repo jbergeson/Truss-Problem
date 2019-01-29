5f13b434614b9d2fdeabec2400bc23870cdb1f53
Jennifer Bergeson <jberges@purdue.edu>
2019-01-29 16:24:57 -0500

    Latest version

--- two_truss.py
+++ two_truss.py
@@ -1,34 +1,7 @@
 import numpy as np 
+from truss import truss
 from openmdao.api import ExplicitComponent, Problem, Group, IndepVarComp, ExecComp, NonlinearBlockGS, NewtonSolver, DirectSolver, ScipyOptimizeDriver
 
-class truss(ExplicitComponent):
-
-    def setup(self):
-
-        self.add_input("A", val = 0.)
-        self.add_input("P", val = 0.)
-
-        self.add_output("sigma", val = 0.)
-
-        # self.declare_partials("sigma", "A")
-        # self.declare_partials("sigma", "P")
-        self.declare_partials("sigma", "A")
-        self.declare_partials("sigma", "P")
-    
-    def compute(self, inputs, outputs):
-        
-        A = inputs["A"]
-        P = inputs["P"]
-
-        outputs["sigma"] = P / (A * 1000000)
-    
-    def compute_partials(self, inputs, J):
-
-        A = inputs["A"]
-        P = inputs["P"]
-
-        J["sigma", "A"] = -P / (1000000 * A ** 2)
-        J["sigma", "P"] = 1 / (A * 1000000)
 
 
 class Truss_Analysis(Group):
@@ -36,13 +9,13 @@ class Truss_Analysis(Group):
     def setup(self):
         
         #F is in N, A is in m^2, L is in m, and sigma is in MPa
-        F = 4
+        F = 4 * 10 ** 7
         indeps = self.add_subsystem("indeps", IndepVarComp())
-        indeps.add_output("P1", -F)
-        indeps.add_output("P2", F)
-        indeps.add_output("A1")
-        indeps.add_output("A2")
-        indeps.add_output("L")
+        indeps.add_output("P1", -F, desc = "Force in member 1")
+        indeps.add_output("P2", F, desc = "Force in member 2")
+        indeps.add_output("A1", desc = "Cross sectional area of member 1")
+        indeps.add_output("A2", desc = "Cross sectional area of member 2")
+        indeps.add_output("L", desc = "Length of members")
 
         trussAB = self.add_subsystem("trussAB", truss())
         trussBC = self.add_subsystem("trussBC", truss())
@@ -50,10 +23,10 @@ class Truss_Analysis(Group):
         self.connect("indeps.P1", ["trussAB.P"])
         self.connect("indeps.P2", ["trussBC.P"])
         
-        self.add_subsystem("obj_cmp", ExecComp("obj = L * (A1 + A2)", A1 = 1.0, A2 = 1.0, L = 1.0))
+        self.add_subsystem("obj_cmp", ExecComp("obj = L * (A1 + A2)", A1 = 1.0, A2 = 1.0,  L = 1.0))
         self.add_subsystem("con1", ExecComp("con = 400 - abs(sigma)"))
         self.add_subsystem("con2", ExecComp("con = 400 - abs(sigma)"))
-
+        # What about the units for the subsystem?
         self.connect("indeps.L", ["obj_cmp.L"])
         self.connect("trussAB.sigma", ["con1.sigma"])
         self.connect("trussBC.sigma", ["con2.sigma"])
@@ -75,7 +48,7 @@ prob.model.add_constraint("con1.con", lower = 0)
 prob.model.add_constraint("con2.con", lower = 0)
 
 prob.setup()
-prob.check_partials(compact_print = True)
+# prob.check_partials(compact_print = True)
 prob.set_solver_print(level = 0)
 
 prob.model.approx_totals()
@@ -83,12 +56,11 @@ prob.model.approx_totals()
 prob.run_driver()
 
 print("minimum found at")
-print(prob["indeps.A1"])
-print(prob["indeps.P1"])
-print(prob["indeps.A2"])
-print(prob["indeps.P2"])
-print(prob["con1.con"])
-print(prob["con2.con"])
+print("A1 = ", prob["indeps.A1"])
+print("P1 = ", prob["indeps.P1"])
+print("A2 = ", prob["indeps.A2"])
+print("P2 = ", prob["indeps.P2"])
+
 
 
 
