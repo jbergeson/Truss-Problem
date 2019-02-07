3264d9c3772e263f317d7abc0958b987366c2595
Bergeson <jbergeso@ndc.nasa.gov>
2019-02-07 13:10:18 -0500

    Next update to verison 3, still doesn't run, in the debugging phase

--- truss_V3.py
+++ truss_V3.py
@@ -1,7 +1,6 @@
 import numpy as np 
-from openmdao.api import ExplicitComponent, Problem, Group, IndepVarComp, ExecComp, NonlinearBlockGS, NewtonSolver, DirectSolver, ScipyOptimizeDriver
+from openmdao.api import ExplicitComponent, ImplicitComponent, Problem, Group, IndepVarComp, ExecComp, NonlinearBlockGS, NewtonSolver, DirectSolver, ScipyOptimizeDriver
 import math
-from openmdao.api import ExplicitComponent, Problem, Group, IndepVarComp, ExecComp, NonlinearBlockGS, NewtonSolver, DirectSolver, ScipyOptimizeDriver
 
 class truss(ExplicitComponent):
 
@@ -30,19 +29,29 @@ class truss(ExplicitComponent):
         J["sigma", "A"] = -P / (1000000 * A ** 2)
         J["sigma", "P"] = 1 / (A * 1000000)
 
-class Node(ExplicitComponent):
-    
+class Node(ImplicitComponent):
+ 
     def initialize(self):
-        self.options.declare("n_forces")
-        self.options.declare("n_external")
+        self.options.declare("n_new_forces", default = 0)
+        self.options.declare("n_old_forces", default = 0)
+        self.options.declare("n_external", default = 0)
 
     def setup(self):
         
-        for n in range(self.options["n_forces"]):
-            n_force = "force{}".format(n)
-            n_direction = "direction{}".format(n)
-            self.add_input(n_direction, units = "rad", desc = "Direction of nth force on node")
-            self.add_output(n_force, units = "N", desc = "Magnitude of nth force on node")
+        self.add_output("x", val = 0)
+        self.add_output("y", val = 0)
+        for n in range(self.options["n_new_forces"]):
+            n_force = "force{}_new".format(n)
+            n_direction = "direction{}_new".format(n)
+            self.add_input(n_direction, units = "rad", desc = "Direction of nth new force on node")
+            self.add_output(n_force, units = "N", desc = "Magnitude of nth new force on node")
+
+        for n in range(self.options["n_old_forces"]):
+            n_force = "force{}_old".format(n)
+            n_direction = "direction{}_old".format(n)
+            self.add_input(n_direction, units = "rad", desc = "Direction of nth old force on node")
+            self.add_input(n_force, units = "N", desc = "Magnitude of nth old force on node")
+
 
         for n in range(self.options["n_external"]):
             force = "external_force{}".format(n)
@@ -59,18 +68,30 @@ class Node(ExplicitComponent):
 
         residuals["x"] = 0
         residuals["y"] = 0
-        for n in range(self.options["n_forces"]):
-            residuals["x"] += outputs["force{}".format(n)] * math.cos(inputs["direction{}".format(n)])
-            residuals["y"] += outputs["force{}".format(n)] * math.sin(inputs["direction{}".format(n)])
+        for n in range(self.options["n__new_forces"]):
+            residuals["x"] += outputs["force{}_new".format(n)] * math.cos(inputs["direction{}_new".format(n)])
+            residuals["y"] += outputs["force{}_new".format(n)] * math.sin(inputs["direction{}_new".format(n)])
+
+        for m in range(self.options["n_old_forces"]):
+            residuals["x"] += inputs["force{}_old".format(m)] * math.cos(inputs["direction{}_old".format(m)])
+            residuals["y"] += inputs["force{}_old".formal(m)] * math.sin(inputs["direction{}_old".format(m)])
         
         for m in range(self.options["n_external"]):
             residuals["x"] += inputs["external_force{}".format(m)] * math.cos(inputs["external_direction{}".format(m)])
             residuals["y"] += inputs["external_force{}".formal(m)] * math.sin(inputs["external_direction{}".format(m)])
             
     def linearize(inputs, outputs, partials):
-        for n in range(self.options["n_forces"]):
-            n_force = "force{}".format(n)
-            n_direction = "direction{}".format(n)
+        for n in range(self.options["n_new_forces"]):
+            n_force = "force{}_new".format(n)
+            n_direction = "direction{}_new".format(n)
+            partials["x", n_force] = math.cos(inputs[n_direction])
+            partials["x", n_direction] = -outputs[n_force] * math.sin(inputs[n_direction])
+            partials["y", n_force] = math.sin(inputs[n_direction])
+            partials["y", n_direction] = outputs[n_force] * math.cos(inputs[n_direction])
+
+        for n in range(self.options["n_old_forces"]):
+            n_force = "force{}_old".format(n)
+            n_direction = "direction{}_old".format(n)
             partials["x", n_force] = math.cos(inputs[n_direction])
             partials["x", n_direction] = -outputs[n_force] * math.sin(inputs[n_direction])
             partials["y", n_force] = math.sin(inputs[n_direction])
