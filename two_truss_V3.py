063c2a75d138c37fb397544e48bd3bfbc87ee90a
Bergeson <jbergeso@ndc.nasa.gov>
2019-02-12 08:08:34 -0500

    Updated to work with truss_V3.py

--- two_truss_V3.py
+++ two_truss_V3.py
@@ -1,10 +1,7 @@
 import math
 import numpy as np 
-from truss_V2 import truss, Node
-from truss import truss
-from openmdao.api import ExplicitComponent, Problem, Group, IndepVarComp, ExecComp, NonlinearBlockGS, NewtonSolver, DirectSolver, ScipyOptimizeDriver
-
-
+from truss_V3 import truss, Node
+from openmdao.api import ExplicitComponent, Problem, Group, IndepVarComp, ExecComp, NonlinearBlockGS, NewtonSolver, DirectSolver, ScipyOptimizeDriver, ArmijoGoldsteinLS
 
 class Truss_Analysis(Group):
     
@@ -12,52 +9,61 @@ class Truss_Analysis(Group):
         
         #F is in N, A is in m^2, L is in m, and sigma is in MPa
         F = 4 * 10 ** 7
+        
         indeps = self.add_subsystem("indeps", IndepVarComp())
-        indeps.add_output("x_reaction_n1", F * ((3 ** .5) / 2), units = "N", desc = "Horizontal reaction force of pinned joint on node 1")
-        indeps.add_output("y_reaction_n1", F / 2, units = "N", desc = "Vertical reaction force of pinned joint on node 1")
-        indeps.add_output("x_reaction_direction_n1", math.pi, units = "rad", desc = "Direction of horizontal reaction force of pinned joint on node 1")
-        indeps.add_output("y_reaction_direction_n1", math.pi / 2, units = "rad", desc = "Direction of vertical reaction force of pinned joint on node 1")
-        indeps.add_output("x_reaction_n2", F * ((3 ** .5) / 2), units = "N", desc = "Horizontal reaction force of pinned joint on node 2")
-        indeps.add_output("y_reaction_n2", F / 2, units = "N", desc = "Vertical reaction force of pinned joint on node 2")
-        indeps.add_output("x_reaction_direction_n2", 0, units = "rad", desc = "Direction of horizontal reaction force of pinned joint on node 2")
-        indeps.add_output("y_reaction_direction_n2", math.pi / 2, units = "rad", desc = "Direction of vertical reaction force of pinned joint on node 2")
-        indeps.add_output("truss1_n1", math.pi * 11 / 6, units = "rad", desc = "Direction of truss1 at node1")
-        indeps.add_output("truss2_n2", math.pi / 6, units = "rad", desc = "Direction of truss2 at node2")
-        
-        indeps.add_output("A1", desc = "Cross sectional area of member 1")
-        indeps.add_output("A2", desc = "Cross sectional area of member 2")
-        indeps.add_output("L", desc = "Length of members")
+        indeps.add_output("n0_x_reaction", math.pi, units = "rad", desc = "Direction of horizontal reaction force of pinned joint on node 0")
+        indeps.add_output("n0_y_reaction", math.pi / 2, units = "rad", desc = "Direction of vertical reaction force of pinned joint on node 0")
+        indeps.add_output("n1_x_reaction", 0, units = "rad", desc = "Direction of horizontal reaction force of pinned joint on node 1")
+        indeps.add_output("n1_y_reaction", math.pi / 2, units = "rad", desc = "Direction of vertical reaction force of pinned joint on node 1")
+        indeps.add_output("n0_truss0", math.pi * 11 / 6, units = "rad", desc = "Direction of truss 0 at node 0")
+        indeps.add_output("n1_truss1", math.pi / 6, units = "rad", desc = "Direction of truss 1 at node 1")
+        indeps.add_output("n2_truss1", math.pi * 7 / 6, units = "rad", desc = "Direction of truss 1 at node 2")
+        indeps.add_output("n2_truss0", math.pi * 5 / 6, units = "rad", desc = "Direction of truss 0 at node 2")
+        indeps.add_output("ext", F, units = "N", desc = "Force applied to truss structure")
+        indeps.add_output("ext_direction", math.pi * 3 / 2, units = "rad", desc = "Direction of force applied to truss structure")
 
-        self.add_subsystem("node1", Node(n_known = 2, n_unknown = 1))
-        self.add_subsystem("node2", Node(n_known = 2, n_unknown = 1))
+        indeps.add_output("A0")
+        indeps.add_output("A1")
+        indeps.add_output("L1")
+        
+        cycle = self.add_subsystem("cycle", Group())
+        cycle.add_subsystem("node0", Node(n_forces_out = 2, n_forces_in = 1))
+        cycle.add_subsystem("node1", Node(n_forces_out = 2, n_forces_in = 1))
+        cycle.add_subsystem("node2", Node(n_forces_out = 2, n_forces_in = 0, n_external_forces = 1))
+        self.add_subsystem("truss0", truss())
         self.add_subsystem("truss1", truss())
-        self.add_subsystem("truss2", truss())
 
-        self.connect("indeps.x_reaction_n1", "node1.known_force 1")
-        self.connect("indeps.y_reaction_n1", "node1.known_force 2")
-        self.connect("indeps.x_reaction_direction_n1", "node1.old_direction 1")
-        self.connect("indeps.y_reaction_direction_n1", "node1.old_direction 2")
-        self.connect("indeps.truss1_n1", "node1.new_direction 1")
-
-        self.connect("indeps.x_reaction_n2", "node2.known_force 1")
-        self.connect("indeps.y_reaction_n2", "node2.known_force 2")
-        self.connect("indeps.x_reaction_direction_n2", "node2.old_direction 1")
-        self.connect("indeps.y_reaction_direction_n2", "node2.old_direction 2")
-        self.connect("indeps.truss2_n2", "node2.new_direction 1")
-
-        self.connect("node1.new_truss 1", ["truss1.P"])
-        self.connect("node2.new_truss 1", ["truss2.P"])
+        #Node 0 connections
+        self.connect("indeps.n0_x_reaction", "cycle.node0.direction0_out")
+        self.connect("indeps.n0_y_reaction", "cycle.node0.direction1_out")
+        self.connect("indeps.n0_truss0", "cycle.node0.direction0_in")
+        #Node 1 connections
+        self.connect("indeps.n1_x_reaction", "cycle.node1.direction0_out")
+        self.connect("indeps.n1_y_reaction", "cycle.node1.direction1_out")
+        self.connect("indeps.n1_truss1", "cycle.node1.direction0_in")
+        #Node 2 connections
+        self.connect("indeps.n2_truss1", "cycle.node2.direction0_out")
+        self.connect("indeps.n2_truss0", "cycle.node2.direction1_out")
+        self.connect("indeps.ext_direction", "cycle.node2.direction0_external")
+        self.connect("indeps.ext", "cycle.node2.force0_external")
+        #Inter-node connections
+        self.connect("cycle.node2.force0_out", ["cycle.node1.force0_in", "truss1.P"])
+        self.connect("cycle.node2.force1_out", ["cycle.node0.force0_in", "truss0.P"])
         
-        self.add_subsystem("obj_cmp", ExecComp("obj = L * (A1 + A2)", A1 = 1.0, A2 = 1.0,  L = 1.0))
-        self.add_subsystem("con1", ExecComp("con = 400 - abs(sigma)"))
-        self.add_subsystem("con2", ExecComp("con = 400 - abs(sigma)"))
-        # What about the units for the subsystem?
-        self.connect("indeps.L", ["obj_cmp.L"])
-        self.connect("truss1.sigma", ["con1.sigma"])
-        self.connect("truss2.sigma", ["con2.sigma"])
-        self.connect("indeps.A1", ["truss1.A", "obj_cmp.A1"])
-        self.connect("indeps.A2", ["truss2.A", "obj_cmp.A2"])
+        cycle.nonlinear_solver = NewtonSolver()
+        self.nonlinear_solver.options["iprint"] = 2
+        cycle.linear_solver = DirectSolver()
 
+
+        self.add_subsystem("obj_cmp", ExecComp("obj = L1 * (A0 + A1)", A0 = 0.0, A1 = 0.0, L1 = 1.0))
+        self.add_subsystem("con0", ExecComp("con = 400 - abs(sigma)"))
+        self.add_subsystem("con1", ExecComp("con = 400 - abs(sigma)"))
+
+        self.connect("indeps.L1", ["obj_cmp.L1"])
+        self.connect("truss0.sigma", ["con0.sigma"])
+        self.connect("truss1.sigma", ["con1.sigma"])
+        self.connect("indeps.A0", ["truss0.A", "obj_cmp.A0"])
+        self.connect("indeps.A1", ["truss1.A", "obj_cmp.A1"])
 
 if __name__ == "__main__":
 
@@ -68,14 +74,14 @@ if __name__ == "__main__":
     prob.driver.options["optimizer"] = "SLSQP"
     # prob.driver.options["tol"] = 1e-8
 
-    prob.model.add_design_var("indeps.A1", lower = .001, upper = 100)
-    prob.model.add_design_var("indeps.A2", lower = .001, upper = 100)
+    prob.model.add_design_var("indeps.A0", lower = 0.001, upper = 100)
+    prob.model.add_design_var("indeps.A1", lower = 0.001, upper = 100)
     prob.model.add_objective("obj_cmp.obj")
+    prob.model.add_constraint("con0.con", lower = 0)
     prob.model.add_constraint("con1.con", lower = 0)
-    prob.model.add_constraint("con2.con", lower = 0)
 
     prob.setup()
-    # prob.check_partials(compact_print = True)
+    prob.check_partials(compact_print = True)
     prob.set_solver_print(level = 0)
 
     prob.model.approx_totals()
@@ -83,9 +89,11 @@ if __name__ == "__main__":
     prob.run_driver()
 
     print("minimum found at")
+    print("A0 = ", prob["indeps.A0"])
+    print("truss0.P", prob["truss0.P"])
     print("A1 = ", prob["indeps.A1"])
     print("truss1.P", prob["truss1.P"])
-    print("A2 = ", prob["indeps.A2"])
-    print("truss2.P", prob["truss2.P"])
-
-
+    print("n0_x_reaction", prob["indeps.n0_x_reaction"])
+    print("n0_y_reaction", prob["indeps.n0_y_reaction"])
+    print("n1_x_reaction", prob["indeps.n1_x_reaction"])
+    print("n1_y_reaction", prob["indeps.n1_y_reaction"])
